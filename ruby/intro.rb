5483eeac0479f121fb8d17bd7ca241dd81ca0f2c
gulnisaslan <gulnisaslan@gmail.com>
2022-11-23 17:14:19 +0300

    added ruby file

---
+++ ruby/intro.rb
@@ -0,0 +1,10 @@
+#Intro
+# -1-print komutu alt satıra geçiş sağlamıyor. yan yana yazılmasını sağlıyor
+print "merhaba dünya\n" 
+
+#puts komutu alt satıra geçiş sağlıyor. alt alta yazılmasını sağlıyor
+puts "merhaba dünya"
+puts "merhaba anne"
+
+#5:13
+
